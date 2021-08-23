c2fec4c6cba439e04ed98a04ce07a2b192fb003e
Vlad <envydef@gmail.com>
2021-08-24 00:53:41 +0300

    feat: CRA for BlackWall

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
+  <BrowserRouter basename=''>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
